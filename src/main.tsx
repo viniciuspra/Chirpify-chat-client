91afef86befd0bed8b28cc4fa363dec5618a4e01
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-21 17:57:54 -0300

    chore: Rename App to Chat and create pages folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { App } from "./App.tsx";
+import { ThemeProvider } from "./components/theme-provider";
+import { AppRoutes } from "./routes";
 import "./index.css";
-
-import { ThemeProvider } from "./components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <AppRoutes />
     </ThemeProvider>
   </React.StrictMode>
 );
