ffa30402359f42251513e6d73daf9d06ad5f95ce
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-21 18:05:31 -0300

    feat: Create loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,9 @@
+import { AnimatedSVG } from "./animatedSvg";
+
+export function Loading() {
+  return (
+    <div className="w-full h-screen flex-1 flex items-center justify-center">
+      <AnimatedSVG />
+    </div>
+  );
+}
