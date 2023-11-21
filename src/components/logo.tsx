491521c10c1a4ff25404f7641fecb5a706a5fe93
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-21 18:04:53 -0300

    feat: Create logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,10 @@
+import logo from "@/assets/logo.svg";
+
+export function Logo() {
+  return (
+    <div className="flex items-center justify-center gap-3 ">
+      <img src={logo} alt="logo chirpify" className="w-24" />
+      <h1 className="font-bold uppercase text-4xl text-white">Chirpify</h1>
+    </div>
+  );
+}
