d8682a4437ccbbdc1bbb9a0f4735c8e3abc8b58b
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-25 18:35:50 -0300

    refactor: Replace default label with Label component

--- src/components/label-input.tsx
+++ src/components/label-input.tsx
@@ -1,5 +1,6 @@
 import { InputHTMLAttributes } from "react";
 import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 interface LabelInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -9,11 +10,11 @@ interface LabelInputProps extends InputHTMLAttributes<HTMLInputElement> {
 export function LabelInput({ label, name, ...rest }: LabelInputProps) {
   return (
     <div className="flex flex-col gap-2">
-      <label htmlFor={name} className="font-medium text-lg">
+      <Label htmlFor={name} className="font-medium text-lg">
         {label}
-      </label>
+      </Label>
       <Input
-        name={name}
+        id={name}
         {...rest}
         className="h-12 rounded-lg outline-none p-3 font-medium bg-primary text-black"
       />
