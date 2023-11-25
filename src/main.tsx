17ab30d8751b78e27389378a5872f7527826ec1f
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-25 18:32:07 -0300

    feat: Add Redux Provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "./components/theme-provider";
 import { AppRoutes } from "./routes";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AppRoutes />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <AppRoutes />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
