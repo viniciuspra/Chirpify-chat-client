f233888369e24cd23d48ef91f329b2979e324705
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-25 18:39:35 -0300

    feat: Add authentication check during app initialization

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,29 @@
+import { FC, Suspense, lazy, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Suspense, lazy } from "react";
-import { FC } from "react";
 import { Loading } from "@/components/loading";
+
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { login, selectAuth } from "@/redux/user/slice";
+import { api } from "@/services/api";
 
 const ChatRoutes: FC = lazy(() => import("./chat.routes"));
 const AuthRoutes: FC = lazy(() => import("./auth.routes"));
 
 export const AppRoutes: FC = () => {
-  let user
+  const user = useAppSelector(selectAuth);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const user = localStorage.getItem("@Chirpify:user");
+    const token = localStorage.getItem("@Chirpify:token");
+
+    if (user && token) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      dispatch(login({ user: JSON.parse(user), token }));
+    }
+  }, [dispatch]);
   return (
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
