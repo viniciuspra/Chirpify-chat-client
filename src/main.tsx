8e7406955e5d41b028f290e627a636f7a9d7fa49
viniciuspra <vinicius.cascaesp@gmail.com>
2023-11-28 18:40:34 -0300

    chore: Change theme key name in localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { store } from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="dark" storageKey="chirpify-ui-theme">
         <AppRoutes />
       </ThemeProvider>
     </Provider>
