5cd5f6d1a6325ed58449fa348be368484cd0d9f3
viniciuspra <vinicius.cascaesp@gmail.com>
2023-12-12 18:11:53 -0300

    feat: Add two new functions to panel reducer

--- src/redux/panel/slice.ts
+++ src/redux/panel/slice.ts
@@ -1,13 +1,16 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { TooltipContent } from "@/components/side-bar";
 import { RootState } from "../store";
+import { UserType } from "@/components/chats-panel";
 
 interface PanelsState {
-  activePanel: TooltipContent;
+  activePanel: TooltipContent | null;
+  selectedUser: UserType | null;
 }
 
 const initialState: PanelsState = {
   activePanel: "Chats",
+  selectedUser: null,
 };
 
 export const panelsSlice = createSlice({
@@ -17,10 +20,16 @@ export const panelsSlice = createSlice({
     setActivePanel(state, action: PayloadAction<TooltipContent>) {
       state.activePanel = action.payload;
     },
+    clearActivePanel(state) {
+      state.activePanel = null;
+    },
+    setSelectedUser(state, action: PayloadAction<UserType | null>) {
+      state.selectedUser = action.payload;
+    },
   },
 });
 
-export const { setActivePanel } = panelsSlice.actions;
+export const { setActivePanel, clearActivePanel, setSelectedUser } = panelsSlice.actions;
 
 export const selectActivePanel = (state: RootState) => state.panels.activePanel;
 
