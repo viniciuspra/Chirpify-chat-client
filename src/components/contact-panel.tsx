82d7fa8c6c68dfb8d760175ba5c6b9889c73488d
viniciuspra <vinicius.cascaesp@gmail.com>
2024-02-01 17:53:47 -0300

    refactor: Modify contact-panel

--- src/components/contact-panel.tsx
+++ src/components/contact-panel.tsx
@@ -1,46 +1,22 @@
-import { useEffect, useState } from "react";
-
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "@/components/ui/popover";
-import {
-  Command,
-  CommandInput,
-  CommandList,
-  CommandEmpty,
-  CommandGroup,
-  CommandItem,
-} from "@/components/ui/command";
-import { Button } from "@/components/ui/button";
 import { ContactCard } from "@/components/contact-card";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
-import { Plus, Search } from "lucide-react";
+import { Search } from "lucide-react";
+import { useEffect, useState } from "react";
+import { UserType } from "./chats-panel";
 import { socket } from "@/services/socket";
-
-type UserType = {
-  id: string;
-  username: string;
-  isOnline: boolean;
-  avatar?: string;
-};
+import { useAppSelector } from "@/redux/hooks";
+import { selectAuth } from "@/redux/auth/slice";
 
 export function ContactPanel() {
-  const [open, setOpen] = useState(false);
   const [users, setUsers] = useState<UserType[]>([]);
-  const [newChat, setNewChat] = useState<UserType | null>(null);
-  const [loading, setLoading] = useState(false);
-  console.log(newChat);
+  const [loading, setLoading] = useState(true);
 
-  const handleUserClick = (user: UserType) => {
-    setNewChat(user);
-  };
+  const currentUser = useAppSelector(selectAuth);
 
   const handleInputChange = (newSearch: string) => {
-    socket.emit("search", newSearch);
+    setLoading(true);
+    socket.emit("searchUser", newSearch, currentUser?.id);
   };
 
   useEffect(() => {
@@ -49,94 +25,58 @@ export function ContactPanel() {
       setLoading(false);
     };
 
-    const handleLoading = () => {
-      setLoading(true);
-    };
+    socket.emit("searchUser", "", currentUser?.id);
 
-    socket.on("search", handleSearch);
-    socket.on("loading", handleLoading);
+    socket.on("searchUser", handleSearch);
 
     return () => {
-      socket.off("search", handleSearch);
-      socket.off("loading", handleLoading);
+      socket.off("searchUser", handleSearch);
     };
-  }, []);
+  }, [currentUser]);
 
   return (
     <div className="bg-secondary/70 min-w-[192px] sm:w-[349px] ml-2 rounded-md shadow-lg flex flex-col border">
-      <header className="flex items-center justify-between h-24 border-b border-muted px-4">
-        <h1 className="flex items-start gap-2 font-semibold text-3xl">Chat</h1>
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            <Button
-              className="rounded-full p-1 w-9 h-9 bg-logo"
-              variant={"outline"}
-            >
-              <Plus />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="p-0" side="left" align="start">
-            <Command>
-              <CommandInput
-                placeholder="search contacts..."
-                onValueChange={handleInputChange}
-              />
-              <CommandList>
-                <CommandEmpty>
-                  {loading ? "loading..." : "No contacts found."}
-                </CommandEmpty>
-                <CommandGroup>
-                  {users.map((user) => (
-                    <button
-                      className="w-full"
-                      key={user.id}
-                      onClick={() => handleUserClick(user)}
-                    >
-                      <CommandItem
-                        value={user.username}
-                        className="cursor-pointer"
-                      >
-                        <div className="flex items-center gap-3">
-                          <div className="relative">
-                            <div
-                              className={`${
-                                user.isOnline ? "bg-green-500" : "bg-red-500"
-                              } h-2 w-2 rounded-full absolute bottom-0 right-0 z-10`}
-                            ></div>
-                            <Avatar>
-                              <AvatarImage src="https://github.com/shadcn.png" />
-                              <AvatarFallback className="uppercase">
-                                {user.username[0] + user.username[1]}
-                              </AvatarFallback>
-                            </Avatar>
-                          </div>
-                          <span className="font-semibold text-base">
-                            {user.username}
-                          </span>
-                        </div>
-                      </CommandItem>
-                    </button>
-                  ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
+      <header className="flex items-center justify-between h-24 border-b border-muted px-4 bg-slate-500/10">
+        <h1 className="flex items-start gap-2 font-semibold text-3xl">
+          Contacts
+        </h1>
       </header>
-      <div className="flex flex-col p-4 max-h-[90%] overflow-hidden">
+      <div className="flex flex-col p-4 max-h-[85%] overflow-hidden">
         <div className="flex items-center gap-3 h-12 border-none bg-accent/30 px-4 rounded-md text-lg mt-2 mb-6">
           <Search />
           <input
             type="text"
             className="bg-transparent outline-none"
-            placeholder="Search messages"
+            placeholder="Search contacts..."
+            onChange={(e) => handleInputChange(e.target.value)}
           />
         </div>
         <ScrollArea className="flex flex-col flex-1">
-          <ContactCard />
-          <ContactCard />
-          <ContactCard />
-          <ContactCard />
+          {loading ? (
+            <div className="flex-1 flex flex-col items-center justify-center h-40">
+              <div className="w-12 h-12 animate-spin border-t-3 border-primary rounded-full" />
+            </div>
+          ) : users && users.length > 0 ? (
+            users.map((user) => {
+              if (user.id !== currentUser?.id) {
+                return (
+                  <ContactCard
+                    key={user.id}
+                    id={user.id}
+                    username={user.username}
+                    imageUrl={user.avatar}
+                    onAddClick={() => {}}
+                  />
+                );
+              } else {
+                return;
+              }
+            })
+          ) : (
+            <div className="flex-1 flex flex-col items-center justify-center h-40">
+              <p className="text-lg">Contact not found!</p>
+            </div>
+          )}
           <ScrollBar orientation="vertical" />
         </ScrollArea>
       </div>
