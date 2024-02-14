0551863414c303f3d9623308db07720aa3f1cc17
viniciuspra <vinicius.cascaesp@gmail.com>
2024-02-14 15:59:25 -0300

    Update auth.routes.tsx

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,9 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import { Login } from "@/pages/login";
-import { Register } from "@/pages/register";
+import { Login } from "../pages/login";
+import { Register } from "../pages/register";
 
 export default function AuthRoutes() {
+  const location = useLocation();
+  console.log("path => ", location.pathname);
   return (
     <Routes>
       <Route path="/" element={<Login />} />
