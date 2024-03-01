218de7a2976c143f63672611c1a8f006b4299c76
viniciuspra <vinicius.cascaesp@gmail.com>
2024-03-01 18:40:57 -0300

    feat: Add new themes to the application

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-type Theme = "dark" | "light" | "system";
+export type Theme = "default" | "orange" | "green" | "blue" | "red" | "yellow";
 
 type ThemeProviderProps = {
   children: React.ReactNode;
@@ -14,7 +14,7 @@ type ThemeProviderState = {
 };
 
 const initialState: ThemeProviderState = {
-  theme: "system",
+  theme: "default",
   setTheme: () => null,
 };
 
@@ -22,7 +22,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
   children,
-  defaultTheme = "system",
+  defaultTheme = "default",
   storageKey = "chirpify-ui-theme",
   ...props
 }: ThemeProviderProps) {
@@ -33,17 +33,14 @@ export function ThemeProvider({
   useEffect(() => {
     const root = window.document.documentElement;
 
-    root.classList.remove("light", "dark");
-
-    if (theme === "system") {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-        .matches
-        ? "dark"
-        : "light";
-
-      root.classList.add(systemTheme);
-      return;
-    }
+    root.classList.remove(
+      "default",
+      "orange",
+      "green",
+      "blue",
+      "red",
+      "yellow"
+    );
 
     root.classList.add(theme);
   }, [theme]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { ThemeProvider } from "./components/theme-provider";
-import { AppRoutes } from "./routes";
-import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+
+import { ThemeProvider } from "@/components/theme-provider";
+import { AppRoutes } from "@/routes";
+import { store } from "@/redux/store";
+
+import "@/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider defaultTheme="dark" storageKey="chirpify-ui-theme">
+      <ThemeProvider defaultTheme="default" storageKey="chirpify-ui-theme">
         <AppRoutes />
       </ThemeProvider>
     </Provider>
